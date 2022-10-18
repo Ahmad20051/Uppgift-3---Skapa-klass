83c6cec851ef610bd064bee60ccbc7e86d15315f
Ahmad EL <ahmelx050608@student.jenseneducation.se>
2022-10-18 14:19:21 +0200

    Add project files.

---
+++ Uppgift 3 - Skapa klass/Fågel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Uppgift_3___Skapa_klass
+{
+    //Här skapas alla funktioner fåglarna ska ha. Namn, längd och vikt
+    internal class Fågel
+    {
+        //Construtor gör så att vi får in nya variablar när vi skapar nya fåglar. Mha constructorn behöver vi inte skriva "set namn", "set längd" osv.
+        public Fågel(string nynamn, int nylängd, int nyvikt)
+        {
+            namn = nynamn;
+            längd = nylängd;
+            vikt = nyvikt;
+        }
+
+        //Den funktioner ger tillbaka fågelns attribut
+        string namn;
+        int längd;
+        int vikt;
+        public string getNamn() { return this.namn; }
+        public int getLängd() { return this.längd; }
+        public int getVikt() { return this.vikt; }
+
+        //Den funktionen finns om man vill ändra attributen, tex ändra längden
+        public void setNamn(string namn) { this.namn = namn; }
+        public void setLängd(int cm) { this.längd = cm; }
+        public void setVikt(int g) { this.vikt = g; }
+
+        //Här ska den valda fågeln sjunga "Craw! Craw!"
+        public void sjung()
+        {
+            Console.WriteLine("Craw! Craw!");
+        }
+
+    }
+}
+
---
+++ Uppgift 3 - Skapa klass/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Uppgift_3___Skapa_klass;
+
+//Här skapas namn, längd, vikt för alla fåglar som skapats
+Fågel fågel1 = new Fågel("Jakob", 15, 300);
+Fågel fågel2 = new Fågel("Adam", 10, 250);
+Fågel fågel3 = new Fågel("Gustav", 12, 275);
+fågel2.sjung();
+
+
+
+
+
+
